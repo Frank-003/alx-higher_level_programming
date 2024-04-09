a95a5fe92a230f94875609d8d3105842a8cabe6c
Frankie-003 <oyiengofrankline49@gmail.com>
2024-04-09 14:07:08 -0700

    task 11

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,3 +1,7 @@
 #!/usr/bin/node
-const list = [1, 2, 3, 4, 5];
-module.exports = { list };
+const { list } = require('./100-data.js');
+
+const newList = list.map((value, index) => value * index);
+
+console.log("Initial list:", list);
+console.log("New list:", newList);
