a47196fb65e64a8e05493e40ae183651c7ce3ce1
Frankie-003 <oyiengofrankline49@gmail.com>
2024-04-12 06:07:44 -0700

    task 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -3,25 +3,12 @@
 import MySQLdb
 import sys
 
-def search_state_by_name(username, password, database, state_name):
-    db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
-cursor = db.cursor()
-query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
-    cursor.execute(query, (state_name,))
-    rows = cursor.fetchall()
-    for row in rows:
-        print(row)
-
-    cursor.close()
-    db.close()
     if __name__ == "__main__":
-        if len(sys.argv) != 5:
-        print("Usage: python script.py <username> <password> <database> <state_name>")
-        sys.exit(1)
+        db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+        c = db.cursor()
 
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-    state_name = sys.argv[4]
+        c.execute("SELECT *\
+                    FROM `states` \
+                    WHERE BINARY `name` = '{}'".format{sys.argv[4]))
 
-    search_state_by_name(username, password, database, state_name)
+        [print(state) for state in c.fetchall()]
