d4f10af42e63cad7f538ea297535cf6955cd6c56
Frankie-003 <oyiengofrankline49@gmail.com>
2024-04-12 06:13:13 -0700

    task 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Module that lists all the states from hbtn_0e_0_usa database."""
-import MySQLdb
 import sys
+import MySQLdb
 
     if __name__ == "__main__":
         db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
