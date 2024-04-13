0ccbaeb95d1008fd13cfca69c19e8f3dc21ccda0
Frankie-003 <oyiengofrankline49@gmail.com>
2024-04-13 03:37:59 -0700

    task 5

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -4,52 +4,13 @@
 import MySQLdb
 import sys
 
-def list_cities_by_state(username, password, database, state_name):
-    try:
-        # Connect to MySQL server
-        db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
+if__name__ == "__main__":
+    db = MySQLdb.connect(host='localhost', user=sys.argv[1],passwd=sys.argv[2], db=sys[3], port=3306)
 
-        # Create a cursor object
-        cursor = db.cursor()
-
-        # Execute SQL query to fetch cities of the specified state
-        query = """
-            SELECT cities.id, cities.name, states.name
-            FROM cities
-            JOIN states ON cities.state_id = states.id
-            WHERE states.name = %s
-            ORDER BY cities.id ASC
-        """
-        cursor.execute(query, (state_name,))
-
-        # Fetch all the rows
-        rows = cursor.fetchall()
-
-        # Display the results
-        for row in rows:
-            print(row)
-
-    except MySQLdb.Error as e:
-        print("Error connecting to the database:", e)
-
-    finally:
-        # Close the cursor and database connection
-        if 'cursor' in locals():
-            cursor.close()
-        if 'db' in locals():
-            db.close()
-
-if __name__ == "__main__":
-    # Check if 4 arguments are provided
-    if len(sys.argv) != 5:
-        print("Usage: python script.py <username> <password> <database> <state_name>")
-        sys.exit(1)
-
-    # Retrieve arguments
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-    state_name = sys.argv[4]
-
-    # Call the function to list cities by state
-    list_cities_by_state(username, password, database, state_name)
+    c = db.cursor()
+    c.execute("""SELECT cities.name FROM cities INNER JOIN states ON states.id=cities.state_id WHERE states.name=%s""", (sys.argv[4].))
+    rows = c.fetchall()
+    tmp = list(row[0] for row in row)
+    print(*tmp, sep=", ")
+    c.close()
+    db.close()
