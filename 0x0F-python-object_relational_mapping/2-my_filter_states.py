c936e23acba78e329d09a05a1ff71722a9a1af9e
Frankie-003 <oyiengofrankline49@gmail.com>
2024-04-13 04:21:01 -0700

    task 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """Module that lists all the states from hbtn_0e_0_usa database."""
-import sys
 import MySQLdb
+import sys
 
     if __name__ == "__main__":
         db = MySQLdb.connect(host="localhost", user.sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], port=3306)
         c = db.cursor()
 
-        c.execute("SELECT *FROM `states` WHERE name LIKE BINARY' {}'"
+        c.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'"
                 .format(sys.argv[4]))
         rows = c.fetchall()
         for row in rows:
