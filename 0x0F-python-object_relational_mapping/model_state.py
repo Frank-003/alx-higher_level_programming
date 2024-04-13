d06c7b0a5b1d0c91d86c894756a61180d017928b
Frankie-003 <oyiengofrankline49@gmail.com>
2024-04-13 05:07:15 -0700

    task 6

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Module that  defines the class representing a state in a MYsql database."""
+"""Module that  defines the class representing a state in a MySQL database."""
 
 from sqlalchemy import Column, Integer, String, create_engine
 from sqlalchemy.ext.declarative import declarative_base
@@ -7,7 +7,12 @@ from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 
 class State(Base):
-    __tablename__ = 'states'
+    """Represents a state for MySQL database.
 
+    __tablename__(str): The name of the MySQL table to store table.
+    id (sqlalchemy.integer): The state's id.
+    name (sqlalchemy.string): The state's name."""
+
+    __tablename__ = 'states'
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
