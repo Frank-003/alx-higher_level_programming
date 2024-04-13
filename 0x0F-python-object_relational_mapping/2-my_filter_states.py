f3b181b82aa2aacab4cfdcf587ef45fd681eb702
Frankie-003 <oyiengofrankline49@gmail.com>
2024-04-13 04:26:18 -0700

    task 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,16 +1,31 @@
 #!/usr/bin/python3
 """Module that lists all the states from hbtn_0e_0_usa database."""
-import MySQLdb
 import sys
+import MySQLdb
 
-    if __name__ == "__main__":
-        db = MySQLdb.connect(host="localhost", user.sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], port=3306)
-        c = db.cursor()
+if __name__ == "__main__":
+    if len(sys.argv) != 5:
+        print("Usage: python script.py <username> <password> <database> <state_name>")
+        sys.exit(1)
 
-        c.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'"
-                .format(sys.argv[4]))
-        rows = c.fetchall()
-        for row in rows:
-            print(row)
-            c.close()
-            db.close()
+    username, password, db_name, state_name = sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4]
+
+    # Connect to MySQL server
+    db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=db_name)
+
+    # Create a cursor object
+    cursor = db.cursor()
+
+    # Execute the SQL query
+    cursor.execute("SELECT * FROM states WHERE name = %s ORDER BY id ASC", (state_name,))
+
+    # Fetch all the rows
+    rows = cursor.fetchall()
+
+    # Display the results
+    for row in rows:
+        print(row)
+
+    # Close the cursor and database connection
+    cursor.close()
+    db.close()
