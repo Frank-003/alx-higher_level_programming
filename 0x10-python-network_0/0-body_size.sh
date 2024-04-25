401105fe508ca1152dca35654e657e7498c2c340
Frank-003 <oyiengofrankline49@gmail.com>
2024-04-25 00:40:24 -0700

    task 0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,15 +1,3 @@
 #!/bin/bash
 # Taking request of URL and display size of the body
-
-if [ $# -eq 0 ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
-url=$1
-
-response=$(curl -s -o response_body.tmp -w "%{size_download}" "$url")
-
-echo "Size of the response body: $response bytes"
-
-rm response_body.tmp
+curl -sI "$1" | grep -i Content-Length | awk '{print $2}'
