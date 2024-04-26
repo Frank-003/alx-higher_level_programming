660469876fb94c19cf3c20c4e2848710c7a79e3e
Frank-003 <oyiengofrankline49@gmail.com>
2024-04-26 06:53:47 -0700

    task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,17 +1,14 @@
 #!/usr/bin/python3
-"""Displays the X-Request-Id header variable of a request to a given URL.
+"""Fetches https://intranet.hbtn.io/status."""
 
-Usage: ./1-hbtn_header.py <URL>
-"""
-import sys
 import urllib.request
 
+if __name__ == "__main__":
 
-url = "https://alx-intranet.hbtn.io/status"
-
-with urllib.request.urlopen(url) as response:
-    html = response.read().decode('utf-8')
-
-print("Body response:")
-print("\t- type:", type(html))
-print("\t- content:", html)
+request = urllib.request.Request("https://intranet.hbtn.io/status")
+    with urllib.request.urlopen(request) as response:
+        body = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
