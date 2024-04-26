0c1d2dba8b45b7ae605fd418bbdf36a149536ae5
Frank-003 <oyiengofrankline49@gmail.com>
2024-04-26 06:43:55 -0700

    task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
+"""Displays the X-Request-Id header variable of a request to a given URL.
+
+Usage: ./1-hbtn_header.py <URL>
+"""
+import sys
 import urllib.request
+
 
 url = "https://alx-intranet.hbtn.io/status"
 
